55a46f4a96f211d3cd8c2ac25b39e99a4a7c46e3
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-01-25 11:53:03 +0100

    Day 2 changes

---
+++ Day1/03_iterations.js
@@ -0,0 +1,10 @@
+let aData = [10, 20, 30, 40];
+let uName = 'Pepe';
+
+console.log(aData[1]);
+console.log(uName[2]);
+
+aData[1] = 56;
+console.log(aData[1]);
+
+console.log(Number.MAX_SAFE_INTEGER);
